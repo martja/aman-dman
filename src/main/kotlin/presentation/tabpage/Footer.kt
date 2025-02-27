ec9c313688e3abd8f148e1949813d1cc625b8cf3
Even Rognlien <even.rognlien@gmail.com>
2025-02-27 21:12:18 +0100

    Make clock functional

--- src/main/kotlin/presentation/tabpage/Footer.kt
+++ src/main/kotlin/presentation/tabpage/Footer.kt
@@ -2,24 +2,25 @@ package org.example.presentation.tabpage
 
 import java.awt.FlowLayout
 import java.awt.Graphics
+import java.time.Instant
 import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.Timer
 
-class Footer : JPanel() {
+class Footer : JPanel(FlowLayout(FlowLayout.RIGHT)) {
     private val timeLabel = JLabel("10:00:22")
 
     init {
-        layout = FlowLayout()
-
-
-
         add(timeLabel)
+
+        // Every second, repaint the component
+        Timer(1000) {
+            repaint()
+        }.start()
     }
 
     override fun paintComponent(g: Graphics?) {
         super.paintComponent(g)
-
-
-
+        timeLabel.text = Instant.now().toString().substring(11, 19)
     }
 }
