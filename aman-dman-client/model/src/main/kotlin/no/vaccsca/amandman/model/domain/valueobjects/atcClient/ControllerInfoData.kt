7a3897069a0f23ed47c450bc127a8999b336752c
Even Rognlien <even.rognlien@gmail.com>
2025-10-19 17:50:46 +0200

    Add missing data class

---
+++ aman-dman-client/model/src/main/kotlin/no/vaccsca/amandman/model/domain/valueobjects/atcClient/ControllerInfoData.kt
@@ -0,0 +1,7 @@
+package no.vaccsca.amandman.model.domain.valueobjects.atcClient
+
+data class ControllerInfoData(
+    val callsign: String?,
+    val positionId: String?,
+    val facilityType: String?,
+)
