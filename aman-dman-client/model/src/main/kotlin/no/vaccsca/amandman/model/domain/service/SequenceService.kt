c94adbc37b66e74d74bf10f101e5d7b2186dd568
soest <felix.soest@vatger.de>
2026-01-26 22:31:12 +0100

    Make indep. rwy systems optional parameter with default empty

--- aman-dman-client/model/src/main/kotlin/no/vaccsca/amandman/model/domain/service/SequenceService.kt
+++ aman-dman-client/model/src/main/kotlin/no/vaccsca/amandman/model/domain/service/SequenceService.kt
@@ -161,7 +161,7 @@ object SequenceService {
         currentSequence: Sequence,
         candidates: List<SequenceCandidate>,
         minimumSeparationNm: Double,
-        independentRunwaySystems: List<Set<String>>
+        independentRunwaySystems: List<Set<String>> = emptyList(),
     ): Sequence {
         // Build a map of the latest candidate data by ID
         val latestCandidateData = candidates.associateBy { it.id }
